9dcb97d37168f0ed4f3074e37b32da462a7cd61a
Subba <anthony@linuxacademy.com>
2018-01-13 05:22:18 +0000

    added motd

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,12 +4,18 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+if node['plat_form_family'] == 'rhel'
+  package = 'httpd'
+elsif node['plat_form_family] == 'debian'
+  package = 'apache2'
+
 package 'apache2' do
-  package_name 'httpd'
+  package_name package
   action :install
 end
 
 service 'apache2' do
-  service_name 'httpd'
+  service_name package
   action [:enable, :start]
 end
+
